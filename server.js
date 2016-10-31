8c3f117fa9df0a4a17d7e40521daae394de5e2a8
居正中 <AlanJui.1960@gmail.com>
2016-10-31 10:07:36 +0800

    初始專案

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.set('views', __dirname);
+app.set('view engine', 'jade');
+
+app.get('*', (req, res) => {
+ res.render('index');
+});
+
+const port = process.env.PORT || 3000;
+const ip = process.env.IP || '127.0.0.1';
+app.listen(port, ip, function () {
+  console.log(`Server is listening on http://${ip}:${port}`);
+});
