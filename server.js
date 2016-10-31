deb041ebf2ac77ff32c9d72e07ce7e413299cf10
居正中 <AlanJui.1960@gmail.com>
2016-10-31 15:29:11 +0800

    設定正式環境使用的 Heroku 平台，可搭配使用 MongoLab 提供的 MongoDB 服務

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const JobModel = require('./models/Job');
 
 const app = express();
 
@@ -10,8 +13,34 @@ app.set('view engine', 'jade');
 
 app.use(express.static(`${__dirname}/public`));
 
+//===========================================================
+/**
+ * API
+ */
+app.get('/api/jobs', (req, res) => {
+  JobModel.find({})
+    .exec((err, jobs) => {
+      res.send(jobs);
+    });
+});
+
+/**
+ * Serve Web Page
+ */
 app.get('*', (req, res) => {
  res.render('index');
+});
+
+//===========================================================
+
+// const MONGODB_URI = `mongodb://localhost/jobfinder`;
+const MONGODB_URI = `mongodb://heroku_n2gj9bh5:of89mtqo97mebfdt5a5k7v7iih@ds139327.mlab.com:39327/heroku_n2gj9bh5`;
+
+mongoose.connect(MONGODB_URI);
+const db = mongoose.connection;
+db.once('open', () => {
+  console.log('Connected to MongoDB successfully!');
+  JobModel.seedJobs();
 });
 
 const port = app.get('port');
