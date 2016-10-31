fbb582bbb7fd2810dd26a9a5ca434abe2f65669f
居正中 <AlanJui.1960@gmail.com>
2016-10-31 10:43:28 +0800

    Angular + Bootstrap : 顯示清單型資料

--- public/app/app.js
+++ public/app/app.js
@@ -4,5 +4,14 @@ angular.module('app')
   .controller('testCtrl', function () {
     var vm = this;
 
-    vm.test = 'working!';
+    vm.jobs = [
+      {
+        title: 'Sales Person',
+        description: 'you will fight dragons'
+      },
+      {
+        title: 'Accoutant',
+        description: 'you will use the keyboard'
+      }
+    ];
   });
